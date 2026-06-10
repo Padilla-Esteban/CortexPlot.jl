dbad5c2def33934593f8af25c4fe6a38bad21456
Marco Congedo <marco.congedo@gmail.com>
2026-06-10 10:50:39 +0200

    dev

--- src/CortexPlot.jl
+++ src/CortexPlot.jl
@@ -6,7 +6,6 @@ using Printf
 using Leadfields
 using Xloreta
 using MAT
-using NPZ
 using LinearAlgebra
 using GeometryBasics
 using MeshIO
@@ -16,11 +15,6 @@ const titleFont = "\x1b[38;5;71m"
 const separatorFont = "\x1b[38;5;113m"
 const defaultFont = "\x1b[0m"
 const greyFont = "\x1b[90m"
-
-#Example data
-const example_eeg = joinpath(abspath(@__DIR__, ".."), "Example Data", "synthetic_eeg.npz")
-
-
 
 export 
 cortex_dashboard,
